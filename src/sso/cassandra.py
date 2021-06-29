71e883e2043951b9431903f0108b73916c47efe9
Piotr Grabowski <piotr.grabowski@scylladb.com>
2021-06-29 20:35:20 +0200

    Add support for RAID in Cassandra

--- src/sso/cassandra.py
+++ src/sso/cassandra.py
@@ -7,10 +7,11 @@ from sso.hdr import HdrLogProcessor
 from sso.ssh import SSH
 from sso.cql import wait_for_cql_start
 from sso.util import run_parallel, find_java, WorkerThread, log_important
+from sso.raid import RAID
 
 class Cassandra:
 
-    def __init__(self, cluster_public_ips, cluster_private_ips, seed_private_ip, properties, cassandra_version=None):
+    def __init__(self, cluster_public_ips, cluster_private_ips, seed_private_ip, properties, setup_raid=True, cassandra_version=None):
         self.properties = properties
         self.cluster_public_ips = cluster_public_ips
         self.cluster_private_ips = cluster_private_ips
@@ -21,6 +22,7 @@ class Cassandra:
             self.cassandra_version = properties['cassandra_version']
             
         self.ssh_user = properties['cluster_user']
+        self.setup_raid = setup_raid
         # trigger early detection of missing java.
         find_java(properties)
   
@@ -31,20 +33,21 @@ class Cassandra:
         ssh = self.__new_ssh(ip)
         ssh.update()
         print(f'    [{ip}] Installing Cassandra: started')
-        ssh.install_one('openjdk-8-jdk', 'java-1.8.0-openjdk')
+        ssh.install_one('openjdk-11-jdk', 'java-11-openjdk')
         ssh.install('wget')
         private_ip = self.__find_private_ip(ip)
+        path_prefix = 'cassandra-raid/' if self.setup_raid else './'
         ssh.exec(f"""
             set -e
             
-            if [ -d 'apache-cassandra-{self.cassandra_version}' ]; then
+            if [ -d '{path_prefix}apache-cassandra-{self.cassandra_version}' ]; then
                 echo Cassandra {self.cassandra_version} already installed.
                 exit 0
             fi
             
             wget -q -N https://mirrors.netix.net/apache/cassandra/{self.cassandra_version}/apache-cassandra-{self.cassandra_version}-bin.tar.gz
-            tar -xzf apache-cassandra-{self.cassandra_version}-bin.tar.gz
-            cd apache-cassandra-{self.cassandra_version}
+            tar -xzf apache-cassandra-{self.cassandra_version}-bin.tar.gz -C {path_prefix}
+            cd {path_prefix}apache-cassandra-{self.cassandra_version}
             sudo sed -i \"s/seeds:.*/seeds: {self.seed_private_ip} /g\" conf/cassandra.yaml
             sudo sed -i \"s/listen_address:.*/listen_address: {private_ip} /g\" conf/cassandra.yaml
             sudo sed -i \"s/rpc_address:.*/rpc_address: {private_ip} /g\" conf/cassandra.yaml
@@ -57,15 +60,21 @@ class Cassandra:
 
     def install(self):
         log_important("Installing Cassandra: started")
+        if self.setup_raid:
+            log_important("Installing Cassandra: setting up RAID")
+            raid = RAID(self.cluster_public_ips, self.ssh_user, '/dev/nvme*n1', 'cassandra-raid', 0, self.properties)
+            raid.install()
+            log_important("Installing Cassandra: finished setting up RAID")
         run_parallel(self.__install, [(ip,) for ip in self.cluster_public_ips])
         log_important("Installing Cassandra: done")
         
     def __start(self, ip):
         print(f'    [{ip}] Starting Cassandra: started')
         ssh = self.__new_ssh(ip)
+        path_prefix = 'cassandra-raid/' if self.setup_raid else './'
         ssh.exec(f"""
             set -e
-            cd apache-cassandra-{self.cassandra_version}
+            cd {path_prefix}apache-cassandra-{self.cassandra_version}
             if [ -f 'cassandra.pid' ]; then
                 pid=$(cat cassandra.pid)
                 kill $pid
@@ -88,9 +97,10 @@ class Cassandra:
     def __stop(self, ip):
         print(f'    [{ip}] Stopping Cassandra: started')
         ssh = self.__new_ssh(ip)
+        path_prefix = 'cassandra-raid/' if self.setup_raid else './'
         ssh.exec(f"""
             set -e
-            cd apache-cassandra-{self.cassandra_version}
+            cd {path_prefix}apache-cassandra-{self.cassandra_version}
             if [ -f 'cassandra.pid' ]; then
                 pid=$(cat cassandra.pid)
                 kill $pid
