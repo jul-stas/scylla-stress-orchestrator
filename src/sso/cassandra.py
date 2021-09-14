e98d9f717581afa4ea8d9420392edfe1a821bce5
Karol Baryła <karol.baryla@scylladb.com>
2021-09-14 14:20:01 +0000

    Fixed URL to cassandra archive

--- src/sso/cassandra.py
+++ src/sso/cassandra.py
@@ -45,7 +45,7 @@ class Cassandra:
                 exit 0
             fi
             
-            wget -q -N https://dist.apache.org/repos/dist/release/cassandra/{self.cassandra_version}/apache-cassandra-{self.cassandra_version}-bin.tar.gz
+            wget -q -N https://archive.apache.org/dist/cassandra/{self.cassandra_version}/apache-cassandra-{self.cassandra_version}-bin.tar.gz
             tar -xzf apache-cassandra-{self.cassandra_version}-bin.tar.gz -C {path_prefix}
             
             wget -q https://github.com/criteo/cassandra_exporter/releases/download/2.3.5/cassandra_exporter-2.3.5.jar
